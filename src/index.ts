cc96202b9ba3388d92f3efc0d29dfabe5e11e365
dule <d@miletich.cc>
2019-08-06 20:49:38 +0200

    Add expample

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("hello");
+import mockReact from "./mockReact";
+import Counter from "./Counter";
+
+console.log("Initial render");
+const App = mockReact.render(Counter);
+
+App.click();
+
+console.log("Second render");
+mockReact.render(Counter);
